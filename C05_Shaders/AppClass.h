7109ae614bec694306fb36ab67943ca1f0e404d9
Brody Davison <bbd4327@rit.edu>
2019-09-25 18:07:43 -0400

    refreshed issues w/ VS

--- C05_Shaders/AppClass.h
+++ C05_Shaders/AppClass.h
@@ -32,7 +32,6 @@ class AppClass
 	glm::vec3 m_v3Color = glm::vec3(-1.0f); //Color of shape
 
 	bool m_bRunning = true; //Running Flag
-	bool compColors = false;
 		
 public:
 	/*
--- _Binary/Shaders/BasicColor.fs
+++ _Binary/Shaders/BasicColor.fs
@@ -2,42 +2,16 @@
 
 in vec3 Color;
 
-vec3 CompColor;
 uniform vec3 SolidColor = vec3(-1,-1,-1);
-uniform bool Complement = false;
-uniform vec3 CompOffset = vec3(1,1,1);
 
 out vec4 Fragment;
 
 void main()
 {
+	Fragment = vec4(Color,1);
 	
-	if(Complement)
-	{
-		CompColor = Color;
-		CompColor.r = 1 - CompColor.r;
-		CompColor.g = 1 - CompColor.g;
-		CompColor.b = 1 - CompColor.b;
-		Fragment = vec4(CompColor,1);
-	}
-	else
-	{
-		Fragment = vec4(Color,1);
-	}
-	
-	
-	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0){
-		
-		if(Complement)
-		{
-			CompColor = CompOffset - SolidColor;
-			Fragment = vec4(CompColor, 1);
-		}
-		else
-		{
-			Fragment = vec4(SolidColor, 1);
-		}
-	}
+	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
+		Fragment = vec4(SolidColor, 1);
 
 	return;
 }
